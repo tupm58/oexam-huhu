b081b068041956e68f0cf96b6938e4018749c6d1
tupm58 <kittkatt.liucuxiu@gmail.com>
2017-04-02 18:25:37 +0700

    decorate answer result

--- src/app/config/constant.js
+++ src/app/config/constant.js
@@ -2,6 +2,6 @@
  * Created by MinhTu on 3/31/2017.
  */
 var config = {
-    // basicUrl:  'https://damp-brushlands-61241.herokuapp.com/api/'
-    basicUrl : 'http://localhost:8000/api/'
+    basicUrl:  'https://damp-brushlands-61241.herokuapp.com/api/'
+    // basicUrl : 'http://localhost:8000/api/'
 };
