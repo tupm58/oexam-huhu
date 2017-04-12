32ee10eae63b77822bb28249f2368c11d4b44395
tupm58 <kittkatt.liucuxiu@gmail.com>
2017-04-12 11:47:41 +0700

    socket : show question, change state leaderboard,loading -need to validate PIN -need answer question

--- src/app/controllers/HostController.js
+++ src/app/controllers/HostController.js
@@ -10,12 +10,12 @@
         .module('app')
         .controller('HostController', [
             'quizService',
-            '$scope','Socket','$stateParams',
+            '$scope','Socket','$stateParams','$interval',
             HostController
 
         ]);
 
-    function HostController(quizService , $scope,Socket,$stateParams) {
+    function HostController(quizService , $scope,Socket,$stateParams,$interval) {
         var vm = this;
         vm.gameId = $stateParams.pin;
         var quizId = $stateParams.id;
@@ -23,6 +23,20 @@
         vm.gameState = 'waitingRoom';
 
         vm.listUserInGame = [];
+
+        //get question list
+        var listQuestion = [];
+        init();
+        function init (){
+            quizService.getQuizDetail(quizId)
+                .then(function(res){
+                    listQuestion = res.data.questions;
+                    console.log(listQuestion);
+                }).catch(function(err){
+                console.log(err);
+            });
+        }
+
 
 
         Socket.connect();
@@ -47,8 +61,54 @@
         //leave
 
         vm.startGame = function(){
-            
-        }
+            vm.gameState = 'question';
+            vm.currentQuestion  = listQuestion[0];
+            vm.countdown = 5;
+            Socket.emit('showQuestion',{
+                question : vm.currentQuestion,
+                gameId : vm.gameId
+            });
+            $interval(function() {
+                vm.countdown--;
+            },1000, vm.countdown)
+                .then(function() {
+                    vm.gameState = 'postQuestion';
+                    Socket.emit('showCorrectAnswer',{
+                        gameId : vm.gameId,
+                        correctAnswer : "aaaaaaaaaaaaaaaaaaaaaaa"
+                    })
+                    vm.correctAnswer = vm.currentQuestion.answers.filter(function(answer){
+                        return answer.correct == true;
+                    });
+                    console.log(vm.correctAnswer);
+                });
+        };
+
+        vm.nextQuestion = function(currentQuestion){
+            vm.gameState = 'question';
+            var index = listQuestion.indexOf(currentQuestion);
+            index ++;
+            vm.currentQuestion = listQuestion[index];
+            vm.countdown = 5;
+            $interval(function() {
+                vm.countdown--;
+            },1000, vm.countdown)
+                .then(function() {
+                    vm.gameState = 'postQuestion';
+                    Socket.emit('showLeaderBoard',{
+                        gameId : vm.gameId
+                    });
+                    vm.correctAnswer = vm.currentQuestion.answers.filter(function(answer){
+                            return answer.correct == true;
+                    });
+                    console.log(vm.correctAnswer);
+                });
+            Socket.emit('showQuestion',{
+                question : vm.currentQuestion,
+                gameId : vm.gameId
+            });
+
+        };
         
     }
 
--- src/app/controllers/PlayerController.js
+++ src/app/controllers/PlayerController.js
@@ -8,12 +8,12 @@
         .module('app')
         .controller('PlayerController', [
             'examService', '$stateParams',
-            '$scope', 'Socket',
+            '$scope', 'Socket','$interval',
             PlayerController
 
         ]);
 
-    function PlayerController(examService, $stateParams, $scope, Socket) {
+    function PlayerController(examService, $stateParams, $scope, Socket,$interval) {
         var vm = this;
         vm.gameId = $stateParams.pin;
         vm.gameState = '';
@@ -36,8 +36,34 @@
             vm.playJoinGameMessage = data.message;
         });
         //leave
-        
-        
+
+        //see question
+        Socket.on('showQuestion',function(data){
+            console.log(data);
+            vm.gameState = 'question';
+            vm.currentQuestion = data.question;
+            vm.countdown = 5;
+            $interval(function() {
+                vm.countdown--;
+            },1000, vm.countdown)
+                .then(function() {
+                    vm.gameState = 'postQuestion';
+
+                });
+        });
+        Socket.on('showLeaderBoard',function(data){
+        });
+        //answer question
+        vm.saveAnswer = function (answer){
+            console.log(answer);
+            vm.gameState = 'loadingAnswer';
+            // Socket.emit('sendAnswer',{
+            //     answer: answer,
+            //     gameId: vm.gameId,
+            //    
+            // });
+
+        }
 
     }
 
