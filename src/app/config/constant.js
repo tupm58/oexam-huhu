2ffadd31c28d1db51c9d23824c5b545cdc2f688e
tupm58 <kittkatt.liucuxiu@gmail.com>
2017-04-18 20:17:22 +0700

    edit sth to deploy

--- src/app/config/constant.js
+++ src/app/config/constant.js
@@ -2,7 +2,7 @@
  * Created by MinhTu on 3/31/2017.
  */
 var config = {
-    basicUrl:  'https://damp-brushlands-61241.herokuapp.com/api/',
+    basicUrl:  'https://damp-brushlands-61241.herokuapp.com',
     // basicUrl : 'http://localhost:8000',
     // basicUrl : 'http://192.168.1.99:8000',
     countHost: 20,
