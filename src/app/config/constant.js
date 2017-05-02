cc6e454d74e6e3cecb6b9f831a7c64fc5a23eb80
tupm58 <kittkatt.liucuxiu@gmail.com>
2017-05-03 01:33:44 +0700

    login with FB :(

--- src/app/config/constant.js
+++ src/app/config/constant.js
@@ -1,9 +1,9 @@
 /**
  * Created by MinhTu on 3/31/2017.
- */
+ */e
 var config = {
-    // basicUrl:  'https://damp-brushlands-61241.herokuapp.com',
-    basicUrl : 'http://localhost:8000',
+    basicUrl:  'https://damp-brushlands-61241.herokuapp.com',
+    // basicUrl : 'http://localhost:8000',
     // basicUrl : 'http://192.168.1.99:8000',
     countHost: 20,
     countPlayer : 200
